0e1f7ac5b7d3ef19dced2be95132bf6bca064f50
akienmain <akienmain@gmail.com>
2025-07-27 20:40:51 +0200

    ver2

--- sketch.js
+++ sketch.js
@@ -1,99 +1,40 @@
 let qData = [["","","","",""]];
+let sheetNames = [];
 let index = 0;
 let state = 2;
-let areSheetNamesGot = false;
 let isLoading = false;
-let isLoaded = false;
-let sheetNamesSelect;
 let answerList = [""];
 let answerNum = 0;
 
-const COLOR_1 = 255;
-const COLOR_2 = 200;
-const COLOR_3 = 20;
-const COLOR_4 = 240;
+const COLOR_1 = '#EEEEEE';
+const COLOR_2 = '#CCCCCC';
+const BACKGROUND = '#FFFFFF';
 
-const SHEET_NAMES_SELECT = [40, 80];
-const B_NEXT = [100, 350, 200, 40];
-const B_GET_QUESTION = [260, 10, 100, 100];
-const B_CHOICES = [
-  [40, 260, 140, 40],
-  [220, 260, 140, 40],
-  [40, 310, 140, 40],
-  [220, 310, 140, 40]
-];
+const select = document.getElementById('select');
+const btn_get_question = document.getElementById('btn_get_question');
+
+const accuracy = document.getElementById('accuracy');
+const question = document.getElementById('question');
+const correct = document.getElementById('correct');
+const selected = document.getElementById('selected');
+
+const answerFrame = document.getElementById('answerFrame');
+const nextFrame = document.getElementById('nextFrame');
 
 function preload() {
   getSheetNames();
-  getData();
 }
 
+
 function setup() {
-  createCanvas(400, 400);
+  let canvas = createCanvas(40, 40);
+  canvas.parent('canvas');
 }
 
 function draw() {
-
-  background(COLOR_1);
-  stroke(COLOR_3);
-
-  drawTextBox("Sheet Name", 40, 60, 200, 80);
-  drawButton(B_GET_QUESTION, "GET QUESTION");
-
-  if (isLoaded) {
-
-    if (state == 2) {
-      index = int(random(0, qData.length));
-      state = 0;
-    }
-
-    ratio = qData[index][0] + "/" + qData[index][1];
-    pct = qData[index][0] / qData[index][1];
-    drawTextBox("Accuracy: "+nf(pct*100,0,2)+"% ("+ratio+")", 40, 120, 200, 140);
-    drawTextBox("Q: "+qData[index][2], 40, 160, 360, 240);
-    
-    answerList = split(qData[index][3], ';');
-    for (let i=0; i<answerList.length; i++) {
-      if (state == 0) {
-        drawButton(B_CHOICES[i], answerList[i]);
-      }
-    }
-
-    if (state == 1) {
-      let correctNum = qData[index][4];
-      drawTextBox("Correct: "+answerList[correctNum], 40, 250, 320, 270);
-      drawTextBox("Selected: "+answerList[answerNum], 40, 280, 320, 300);
-      drawTextBox(answerNum==correctNum?"Correct":"Failed", width/2-40, 310, width/2+40, 330);
-      drawButton(B_NEXT, "NEXT");
-    }
-  }
+  background(BACKGROUND);
   if (isLoading) {
     drawLoadingSign();
-  }
-}
-
-function mouseClicked() {
-
-  for (let i=0; i<answerList.length; i++) {
-    if (state == 0 && isInside(B_CHOICES[i])) {
-      answerNum = i;
-      qData[index][1] = str(int(qData[index][1]) + 1);
-      let correctNum = qData[index][4];
-      if (answerNum == correctNum) qData[index][0] = str(int(qData[index][0]) + 1);
-      sendResult(index, qData[index][0], qData[index][1]);
-      state = (state+1) % 3;
-      return;
-    }
-  }
-
-  if (isInside(B_GET_QUESTION)) {
-    getData();
-    return;
-  }
-
-  if (state == 1 && isInside(B_NEXT)) {
-    state = (state+1) % 3;
-    return;
   }
 }
 
@@ -112,29 +53,34 @@ function getUrl(deployId, dict=null) {
 function getDeployId() {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get('deployId');
-  //return **DEPLOY_ID_TEST**;
+  //return "AKfycbzCbTEjcU4s547Ajm3KS2_wcRFjyxXJ0GAI1TEklebh4pJbEuc-vV14-joD-VfSAHmR";
 }
 
 function getData() {
   let url = getUrl(getDeployId(), {
     'func': 'getData',
-    'sheetName': sheetNamesSelect.value()
+    'sheetName': select.options[select.selectedIndex].value
   });
   isLoading = true;
-  isLoaded = false;
   httpGet(url, function(response) {
+    isLoading = false;
+
     print(response);
     qData = strToArray(response);
     index = 0;
-    isLoading = false;
-    isLoaded = true;
+
+    index = randomIndex(qData);
+    question.textContent = qData[index][2];
+    answerList = split(qData[index][3], ';');
+
+    generateButton(answerList);
   });
 }
 
 function sendResult(index, correct, total) {
   let url = getUrl(getDeployId(), {
     'func': 'sendResult',
-    'sheetName': sheetNamesSelect.value(),
+    'sheetName': select.options[select.selectedIndex].value,
     'index': index,
     'correct': correct,
     'total': total
@@ -147,16 +93,18 @@ function getSheetNames() {
     'func': 'getSheetNames'
   });
   isLoading = true;
-  areSheetNamesGot = false;
   httpGet(url, function(response) {
-    print(response);
-    let sheetNames = split(response, ',');
-    sheetNamesSelect = createSelect().position(SHEET_NAMES_SELECT[0], SHEET_NAMES_SELECT[1]);
-    for (let i=0; i<sheetNames.length; i++) {
-      sheetNamesSelect.option(sheetNames[i]);
-    }
     isLoading = false;
-    areSheetNamesGot = true;
+
+    print(response);
+    sheetNames = split(response, ',');
+
+    for (let i=0; i<sheetNames.length; i++) {
+      var option = document.createElement("option");
+      option.text = sheetNames[i];
+      option.value = sheetNames[i];
+      select.add(option);
+    }
   });
 }
 
@@ -182,39 +130,91 @@ function arrayToStr(a) {
   return s;
 }
 
-function drawButton(buttonPos, buttonStr="") {
-  strokeWeight(0);
-  fill(COLOR_2);
-  rect(buttonPos[0], buttonPos[1], buttonPos[2], buttonPos[3], 20);
-  fill(COLOR_3);
-  textAlign(CENTER, CENTER);
-  text(buttonStr, buttonPos[0], buttonPos[1], buttonPos[2], buttonPos[3]);
-  textAlign(LEFT, TOP);
-}
-
-function drawTextBox(tex, x, y, x2, y2) {
-  pad = 4;
-  corner = 4;
-  fill(COLOR_4);
-  rect(x, y, x2-x, y2-y, corner);
-  fill(COLOR_3);
-  text(tex, x+pad, y+pad, x2-pad, y2-pad);
-  textAlign(LEFT, TOP);
-}
-
-function isInside(buttonPos) {
-  return mouseX > buttonPos[0] &&
-    mouseX < buttonPos[0] + buttonPos[2] &&
-    mouseY > buttonPos[1] &&
-    mouseY < buttonPos[1] + buttonPos[3];
-}
-
 function drawLoadingSign() {
   for (let i=0; i<8; i++) {
     push();
     fill(COLOR_2);
-    translate(cos(frameCount/8+i*PI/4)*30,sin(frameCount/6+i*PI/4)*30);
-    circle(width/2, height/2, 10);
+    stroke(COLOR_2);
+    translate(cos(frameCount/8+i*PI/4)*15,sin(frameCount/6+i*PI/4)*15);
+    circle(width/2, height/2, 6);
     pop();
   }
 }
+
+function generateButton(answerList) {
+
+  removeChildren(answerFrame);
+  removeChildren(nextFrame);
+  correct.textContent = "";
+  selected.textContent = "";
+
+  ratio = qData[index][0] + "/" + qData[index][1];
+  pct = qData[index][0] / qData[index][1];
+  accuracy.textContent = "Accuracy: "+nf(pct*100,0,2)+"% ("+ratio+")";
+
+  for (let i=0; i<answerList.length; i++) {
+    const button = document.createElement('button');
+    button.id = `btn_${i}`;
+    button.textContent = answerList[i];
+    button.addEventListener('click', function() {
+
+      removeChildren(answerFrame);
+
+      answerNum = i;
+      qData[index][1] = addNumberStr(qData[index][1], 1);
+      let correctNum = qData[index][4];
+      if (answerNum == correctNum) {
+        qData[index][0] = addNumberStr(qData[index][0], 1);
+      }
+      sendResult(index, qData[index][0], qData[index][1]);
+      state = (state+1) % 3;
+
+      ratio = qData[index][0] + "/" + qData[index][1];
+      pct = qData[index][0] / qData[index][1];
+      accuracy.textContent = "Accuracy: "+nf(pct*100,0,2)+"% ("+ratio+")";
+
+      correct.textContent = "Correct: "+answerList[correctNum];
+      selected.textContent = "Selected: "+answerList[answerNum];
+
+      // next button
+      removeChildren(nextFrame);
+
+      const bt = document.createElement('button');
+      bt.id = 'next';
+      bt.textContent = 'NEXT';
+      bt.addEventListener('click', function() {
+
+        nextFrame.removeChild(bt);
+
+        index = randomIndex(qData);
+        question.textContent = qData[index][2];
+        answerList = split(qData[index][3], ';');
+        generateButton(answerList);
+
+        correct.textContent = "";
+        selected.textContent = "";
+
+      });
+      nextFrame.appendChild(bt);
+    });
+    answerFrame.appendChild(button);
+  }
+}
+
+function addNumberStr(inputStr, additionalInt) {
+  return str(int(inputStr) + additionalInt);
+}
+
+function randomIndex(qData) {
+  return int(random(0, qData.length));
+}
+
+function removeChildren(element) {
+  while (element.firstChild) {
+    element.firstChild.remove()
+  }
+}
+
+btn_get_question.addEventListener('click', () => {
+  getData();
+})
